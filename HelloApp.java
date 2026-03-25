70b366443a1846e3cc15c7cb62514eef92cd6c49
Aavishkar Singh <aavishkarparul@gmail.com>
2026-03-26 04:50:42 +0530

    UC4

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
-        
+
         if (args.length > 0) {
-            // If argument is given
-            System.out.println("Hello " + args[0]);
+            // Join all arguments with space
+            String name = String.join(" ", args);
+            System.out.println("Hello " + name);
         } else {
-            // Default message
             System.out.println("Hello User");
         }
-        
+
     }
 }
